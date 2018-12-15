b73f3254398408275a70367f1d6128847b5a23e2
Kevin Gonzalez <kgonzale@cbu.edu>
2018-12-15 00:13:11 -0600

    fixed async error, added fs.write

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -1,17 +1,18 @@
 const $ = require("cheerio");
 const axios = require("axios");
+const fs = require("fs");
 
-async scrapeCBU = () => {
-    const html = await axios('https://www.cbu.edu/faculty-staff');
-    let faculty = [];
+async function scrapeCBU() {
+  const html = await axios("https://www.cbu.edu/faculty-staff");
+  let faculty = [];
 
-    $('h2.staff-directory-department', html).map(item =>{
-        faculty.push({
-            title: $('staff-member-title', item)
-        });
+  $("h2.staff-directory-department", html).map(item => {
+    faculty.push({
+      title: $("staff-member-title", item)
     });
+  });
 
-    return faculty;
-};
+  fs.writeFile("faculty.json", JSON.stringify(faculty));
+}
 
-
+scrapeCBU();
