42b0231e783bd8642adbd8a9d8b9076d0f6a01fc
Kevin Gonzalez <kgonzale@cbu.edu>
2018-12-16 01:32:08 -0600

    saving before making async function

--- scraper/test.js
+++ scraper/test.js
@@ -2,17 +2,24 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const $ = require("cheerio");
+const axios = require("axios");
 
 app.get("/faculty", (req, res) => {
-  const html = axios("https://www.cbu.edu/faculty-staff");
+  // async function scraper(){
 
-  const scrape = $("h2.staff-directory-department", html).map(item => {
+  // }
+
+  const html = await axios(
+    "http://catalogs.rutgers.edu/generated/nb-ug_current/pg159.html"
+  );
+
+  const scrape = $("div.item-container", html).map(item => {
     return {
-      title: $("staff-member-title", item).text()
+      code: $(".course-annotation", container).text()
     };
   });
 
   res.send(scrape);
-
-  app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 });
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
